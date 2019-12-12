7b4ce78f1cb9c15c012dc0e74efffe1b4e2230b6
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-12-12 14:33:33 -0300

    Fixing services mock

--- src/tests/ServicesList.test.js
+++ src/tests/ServicesList.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import pretty from 'pretty';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import pretty from "pretty";
 
-import ServicesList from '../components/ServicesList';
+import ServicesList from "../components/ServicesList";
 
 let container = null;
 
 beforeEach(() => {
-  container = document.createElement('div');
+  container = document.createElement("div");
   document.body.appendChild(container);
 });
 
@@ -18,11 +18,11 @@ afterEach(() => {
   container = null;
 });
 
-test('renders services list based on props', () => {
+test("renders services list based on props", () => {
   const services = [
-    { serviceType: 'ras', location: 'path/to/ras' },
-    { serviceType: 'as', location: 'path/to/as' },
-    { serviceType: 'fns', location: 'path/to/fns' }
+    { serviceType: "ras", location: "/path/to/ras" },
+    { serviceType: "as", location: "/path/to/as" },
+    { serviceType: "fns", location: "/path/to/fns" }
   ];
 
   act(() => {
@@ -32,21 +32,23 @@ test('renders services list based on props', () => {
   expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
     "<div>
       <ul class=\\"dropdown-menu order-dropdown\\" aria-labelledby=\\"membersDropdown\\">
-        <li class=\\"btn btn-link dropdown-item\\"><a class=\\"btn btn-link\\" href=\\"http://localhost:8081path/to/ras\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Resource Allocation Service</a></li>
-        <li class=\\"btn btn-link dropdown-item\\"><a class=\\"btn btn-link\\" href=\\"http://localhost:8081path/to/as\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Authentication Service</a></li>
-        <li class=\\"btn btn-link dropdown-item\\"><a class=\\"btn btn-link\\" href=\\"http://localhost:8081path/to/fns\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Federated Network Service</a></li>
+        <li class=\\"btn btn-link dropdown-item\\"><a class=\\"btn btn-link\\" href=\\"http://localhost:8081/path/to/ras\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Resource Allocation Service</a></li>
+        <li class=\\"btn btn-link dropdown-item\\"><a class=\\"btn btn-link\\" href=\\"http://localhost:8081/path/to/as\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Authentication Service</a></li>
+        <li class=\\"btn btn-link dropdown-item\\"><a class=\\"btn btn-link\\" href=\\"http://localhost:8081/path/to/fns\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Federated Network Service</a></li>
       </ul>
     </div>"
   `);
 });
 
-test('renders local service path correctly', () => {
-  const services = [{ serviceType: 'local', location: 'http://path.to.local/doc' }];
+test("renders local service path correctly", () => {
+  const services = [
+    { serviceType: "local", location: "http://path.to.local/doc" }
+  ];
 
   act(() => {
     render(<ServicesList services={services} />, container);
   });
 
-  const link = container.querySelector('a');
-  expect(link.innerHTML).toBe('Local Provider Catalog');
+  const link = container.querySelector("a");
+  expect(link.innerHTML).toBe("Local Provider Catalog");
 });
